187186a652af59658579fe2dd386e780cb1ef4c0
Quy Le <quy.le@dinovative.com>
2018-07-31 10:38:12 +0700

    Delete unnecessary resources

--- v2/config/routes.rb
+++ v2/config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v2 do
-      resources :venues
       resources :matches do
         collection do
           post :import
           get :show_player_result
         end
       end
-      resources :tournaments
-      resources :players
-      resources :groups
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
